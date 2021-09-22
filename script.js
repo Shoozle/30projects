bbeab41dc1d284f2428105bb6ed4f35aa414559a
Shoozle <sean@seanmurtagh.uk>
2021-09-22 15:03:08 +0100

    Video player

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 /*
-TASKS SECTION /////////////////////////////////////////////
+VIDEO PLAYER SECTION /////////////////////////////////////////////
 */
 
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() { 
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+function updateButton() {
+    const icon = this.paused ? '►' : '❚❚';
+    toggle.textContent = icon;
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+/*
+TASKS SECTION /////////////////////////////////////////////
+*/
 
 const taskchecks = document.querySelectorAll('.check');
 
@@ -19,14 +57,10 @@ function handleTask(e) {
             }
         })
     }
-
-
-
     lastChecked = this;
 }
 
 taskchecks.forEach(task => task.addEventListener('click', handleTask))
-
 
 /*
 CHECKBOX SECTION /////////////////////////////////////////////
