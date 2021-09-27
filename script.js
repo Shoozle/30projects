ba3541bd9199abac91c6e884d4c4e87fb866bbbb
Shoozle <sean@seanmurtagh.uk>
2021-09-27 03:41:50 +0100

    total timer

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
 /*
+VIDEO TIMES CODE /////////////////////////////////////////////
+*/
+
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return (mins * 60) + secs;
+    })
+    .reduce((total, vidSeconds) => total + vidSeconds);
+
+    let secondsLeft = seconds;
+    const hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
+
+    const mins = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+
+    const trailZeros = new Intl.NumberFormat('en-us', {
+        minimumIntegerDigits: 2
+    })
+
+
+    console.log(trailZeros.format(hours), trailZeros.format(mins), trailZeros.format(secondsLeft))
+
+    const totalTime = document.querySelector('.totalTime')
+    totalTime.innerHTML = `${trailZeros.format(hours)} : ${trailZeros.format(mins)} : ${trailZeros.format(secondsLeft)}`
+
+/*
 BANDS CODE /////////////////////////////////////////////
 */
 
