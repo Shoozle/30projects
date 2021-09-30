52439ce415aa7eebf70ac9bb038f2f46bb114964
Shoozle <sean@seanmurtagh.uk>
2021-09-30 17:03:10 +0100

    Links bg follow

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 /*
+FOLLOW ALONG LINKS RECOGNITION /////////////////////////////////////////////
+*/
+
+const triggers = document.querySelectorAll('.followLink');
+
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    const linkCoords = this.getBoundingClientRect();
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    }
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+
+
+
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
+
+/*
 SPEECH RECOGNITION /////////////////////////////////////////////
 */
 
